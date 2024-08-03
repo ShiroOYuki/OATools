7ae756b8f418dfc0c110f911fb10a5046d357d70
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2024-08-03 17:17:25 +0800

    update v1.1.3

--- OAHint/ex_info.js
+++ OAHint/ex_info.js
@@ -1,6 +1,6 @@
 function load_ex_info() {
     let b = document.querySelector("#dt-stage-show > ul > li:nth-child(2) > a");
-    if (!b) load_student_info();
+    if (!b) load_student_info_embed();
     else {
         b.addEventListener("click", function(e) {
             const observer = new MutationObserver((mutationsList, observer) => {
@@ -26,6 +26,33 @@ function load_student_info() {
         e.addEventListener("click", function () {
             let col = find_parent(this, "tr");
             class_type = col.childNodes[1].firstChild.textContent.split("(")[1].replace(")", "");
+            
+            const observer = new MutationObserver((mutationsList, observer) => {
+                for(let mutation of mutationsList) {
+                    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                        // 在這裡你可以添加其他檢查來確保是你期待的元素已經載入
+                        let label = document.querySelector("#remote_modal h4#myModalLabel");
+                        label.innerHTML = `${label.textContent} (${class_type})`;
+                        // 停止觀察
+                        observer.disconnect();
+                    }
+                }
+            });
+            // 開始觀察 #content 容器的變化
+            observer.observe(document.getElementById('remote_modal'), { childList: true, subtree: true });
+        });
+    });
+    console.log("Corp EX Info Loaded!");
+}
+
+function load_student_info_embed() {
+    let record_btn = document.querySelectorAll("[id^=report_status] > div > a");
+    let class_type = null;
+    record_btn.forEach(function (e) {
+        e.addEventListener("click", function () {
+            let col = find_parent(this, "tr");
+            class_type = col.childNodes[0].textContent.split("(")[1].replace(")", "");
+            console.log(class_type);
             
             const observer = new MutationObserver((mutationsList, observer) => {
                 for(let mutation of mutationsList) {
