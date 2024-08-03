c862f0f0542e44767d3e98ece0d52df6cd19d313
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2024-08-03 14:59:16 +0800

    a

---
+++ OAHint/ex_info.js
@@ -0,0 +1,4 @@
+// function load_ex_info() {
+//     let record_btn = document.querySelectorAll("[id^=report_status] > div > a");
+//     console.log(record_btn);
+// }
--- OAHint/oa_hint.user.js
+++ OAHint/oa_hint.user.js
@@ -7,7 +7,8 @@
 // @match        https://corp.orangeapple.co/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=orangeapple.co
 // @grant        none
-// @require     https://github.com/ShiroOYuki/OATools/raw/main/OAHint/hint.js
+// @require      https://github.com/ShiroOYuki/OATools/raw/main/OAHint/hint.js
+// @require      https://github.com/ShiroOYuki/OATools/raw/main/OAHint/ex_info.js
 // ==/UserScript==
 
 (function() {
@@ -16,5 +17,11 @@
     // Your code here...
     window.addEventListener('load', () => {
         load_hint();
+        load_ex_info();
     });
+
+    function load_ex_info() {
+        let record_btn = document.querySelectorAll("[id^=report_status] > div > a");
+        console.log(record_btn);
+    }
 })();
